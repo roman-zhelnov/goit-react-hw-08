5614e1fe00ddb7676aac80b7eda39e97c5f03118
Roman Zhelnov <roman95zhelnov@gmail.com>
2024-10-20 14:24:55 +0300

    create register and login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,58 @@
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Sign up now!</h1>
+            <p className="py-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+              totam magnam natus ad quasi molestias dolores nesciunt provident
+              nobis! Libero minima officia repellendus deserunt! Quibusdam quia
+              quo magni optio odio.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Register;
